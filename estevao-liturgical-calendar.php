fb072761f3d3d6f7a4f601286aa552431d2b2f80
Douglas Araujo <dodopokcamilo@gmail.com>
2026-03-09 16:41:40 -0300

    chore: bump version

--- estevao-liturgical-calendar.php
+++ estevao-liturgical-calendar.php
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin constants
-define('ESTEVAO_LITURGICAL_VERSION', '1.0.2');
+define('ESTEVAO_LITURGICAL_VERSION', '2.0.0');
 define('ESTEVAO_LITURGICAL_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ESTEVAO_LITURGICAL_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ESTEVAO_LITURGICAL_PLUGIN_BASENAME', plugin_basename(__FILE__));
